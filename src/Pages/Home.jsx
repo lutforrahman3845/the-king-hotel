5a2e27cbaa62f3ecbadc897b0b2136ac779adb1c
Lutfor Rahman <lutforr3845@gmail.com>
2024-12-22 05:17:28 +0600

    contact with us create

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../Components/About";
 import Bannar from "../Components/Bannar";
+import ContactUs from "../Components/ContactUs";
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
             <About></About>
+            <ContactUs></ContactUs>
         </div>
     );
 };
