6bc39da58b8fe7e8e5d6a8b8297f4eae819f4ba3
Lutfor Rahman <lutforr3845@gmail.com>
2024-12-24 21:51:44 +0600

    roomDetails review build

--- src/Pages/RoomDetails.jsx
+++ src/Pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate, useParams } from "react-router-dom";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import LoadingSpinner from "../Components/LoadingSpinner";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -13,30 +13,30 @@ import useAxiosSecure from "../Hooks/useAxiosSecure";
 
 const RoomDetails = () => {
   const navigate = useNavigate();
-  const axiosSecure= useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
   const { id } = useParams();
   const queryClient = useQueryClient();
   const [selectedStartDate, setSelectedStartDate] = useState(null);
   const [selectedEndDate, setSelectedEndDate] = useState(null);
   const [error, setError] = useState("");
   const { user } = useContext(AuthContext);
+  // rewviews
+  const [count, setCount] = useState(0);
+  const rewviewsPerPage = 4;
+  const [currentPage, setCurrentPage] = useState(1);
 
+  // rooms details
   const { data: room, isLoading } = useQuery({
     queryKey: ["roomDetails", id],
     queryFn: async () => {
-      const { data } = await axiosSecure.get(
-        `/room_details/${id}`
-      );
+      const { data } = await axiosSecure.get(`/room_details/${id}`);
       return data;
     },
   });
 
   const { isPending, mutateAsync } = useMutation({
     mutationFn: async (bookingData) => {
-      const { data } = await axiosSecure.post(
-        `/book_room`,
-        bookingData
-      );
+      const { data } = await axiosSecure.post(`/book_room`, bookingData);
       return data;
     },
     onSuccess: () => {
@@ -80,7 +80,7 @@ const RoomDetails = () => {
     if (selectedEndDate && durationInDays > maxDuration) {
       return setError(`You can only book up to ${maxDuration} days`);
     }
-     const days = durationInDays <= 0 ? 1 : durationInDays;
+    const days = durationInDays <= 0 ? 1 : durationInDays;
     // Proceed with booking logic
     mutateAsync({
       roomId: id,
@@ -102,10 +102,35 @@ const RoomDetails = () => {
     slidesToScroll: 1,
     arrows: false,
   };
+  // reviews
+  const { data: review, isLoading: isLoadingReviews } = useQuery({
+    queryKey: ["review", currentPage],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(
+        `/reviews_ofRooms?room=${id}&skip=${currentPage}&limit=${rewviewsPerPage}`
+      );
+      return data;
+    },
+  });
+  useEffect(() => {
+    axiosSecure.get(`/reviews_count?rm=${id}`).then((res) => {
+      setCount(res.data.count);
+    });
+  }, []);
+  const totalPages = Math.ceil(count / rewviewsPerPage);
+  const pages = [...Array(totalPages).keys()].map((i) => i + 1);
+  console.log(totalPages);
+  console.log(review);
+
+  const handlePaginationButton = (value) => {
+    setCurrentPage(value);
+  };
+  // lodaing
   if (isLoading) return <LoadingSpinner />;
+  if (isLoadingReviews) return <LoadingSpinner />;
 
   return (
-    <>
+    <div className="my-8 md:my-14">
       <div className="shadow-lg rounded-lg overflow-hidden bg-white border grid grid-cols-1 md:grid-cols-2 mx-3 my-8 md:my-14 lg:my-16 xl:my-20 overflow-x-hidden gap-6">
         <div className="slider-container">
           <Slider {...settings}>
@@ -166,7 +191,116 @@ const RoomDetails = () => {
           </div>
         </div>
       </div>
-
+      {/* reviews */}
+      {review.length <= 0 ? (
+        <p className="text-center text-gray-600 italic max-w-2xl mx-auto">
+          <span className="mx-4">
+            This room is waiting for its first review! Our customers love
+            sharing their experiences, and we're confident you'll have a
+            memorable stay too. Book now with confidence!
+          </span>
+        </p>
+      ) : (
+        <div className="px-8 md:px-14 lg:px-16 xl:px-20 overflow-x-hidden">
+          <h4 className="text-2xl font-semibold text-indigo-600">Reviews</h4>
+          {review.map((review) => (
+            <div key={review._id} className="mt-8">
+              <div className="flex items-center gap-2">
+                {review?.userPhoto ? (
+                  <img
+                    className="w-12 h-12 object-cover rounded-full"
+                    src={review?.userPhoto}
+                    alt="User imge"
+                    referrerPolicy="no-referrer"
+                  />
+                ) : (
+                  <img
+                    className="w-12 h-12 rounded-full border border-primary p-[2px]"
+                    src={userIcon}
+                    alt="Default User Icon"
+                  />
+                )}
+                <div>
+                  <p className="text-lg font-semibold">{review?.userName}</p>
+                  <p>{review?.timestamp}</p>
+                </div>
+              </div>
+              <div className="my-4 ml-10">
+                <p className="font-medium">
+                  Rating : ⭐ ({review?.rooomRating})
+                </p>
+                <p className="font-medium max-w-3xl">
+                  Comment :{" "}
+                  <span className="text-gray-700 font-normal">
+                    {" "}
+                    {review?.comment}
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
+          <div className="flex justify-center mt-12">
+            {/* Previous Button */}
+            <button
+              disabled={currentPage === 1}
+              onClick={() => handlePaginationButton(currentPage - 1)}
+              className="px-4 py-2 mx-1  disabled:text-gray-200 bg-secondary/70 rounded-md disabled:cursor-not-allowed disabled:bg-gray-400  disabled:hover:text-gray-500 hover:bg-secondary  text-white"
+            >
+              <div className="flex items-center -mx-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                  />
+                </svg>
+              </div>
+            </button>
+            {/* Numbers */}
+            {pages.map((btnNum) => (
+              <button
+                key={btnNum}
+                onClick={() => handlePaginationButton(btnNum)}
+                className={`hidden  ${
+                  currentPage === btnNum ? "bg-indigo-600 text-white" : ""
+                } px-4 py-2 mx-1   rounded-md sm:inline hover:bg-indigo-500  hover:text-white`}
+              >
+                {btnNum}
+              </button>
+            ))}
+            {/* Next Button */}
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => handlePaginationButton(currentPage + 1)}
+              className="px-4 py-2 mx-1   bg-secondary/70 rounded-md hover:bg-secondary disabled:bg-gray-400 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 text-white disabled:cursor-not-allowed disabled:text-gray-200"
+            >
+              <div className="flex items-center -mx-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </div>
+            </button>
+          </div>
+        </div>
+      )}
       {/* Modal */}
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box h-[500px] flex flex-col justify-between p-4">
@@ -216,7 +350,7 @@ const RoomDetails = () => {
           </div>
         </div>
       </dialog>
-    </>
+    </div>
   );
 };
 
