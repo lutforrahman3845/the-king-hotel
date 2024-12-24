35388aa5a1dde8fe350548ed3c43c22aa431636a
Lutfor Rahman <lutforr3845@gmail.com>
2024-12-24 22:59:25 +0600

    create FQ question

---
+++ src/Components/FQquestion.jsx
@@ -0,0 +1,69 @@
+const FQquestion = () => {
+  return (
+    <div className="py-8 md:py-14 lg:py-16  px-5 overflow-x-hidden">
+      <h1 className="text-xl  lg:text-2xl  xl:text-3xl  font-cinzel font-semibold">
+        FAQs for The King Hotel , Matrix
+      </h1>
+      <div className="join join-vertical w-full lg:w-3/5 mt-6">
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-lg font-medium">
+            Does The King Hotel have a pool?
+          </div>
+          <div className="collapse-content">
+            <p>Yes, The King Hotel has an outdoor pool.</p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-lg font-medium">
+            How much is parking at The King Hotel?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Complimentary parking is available at The King Hotel for hotel
+              guests.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-lg font-medium">
+            Can I cancel my booking at The King Hotel for a full refund?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Always check the hotel's cancellation policy before booking a
+              rate. Room cancellation is subject to rate booked at time of
+              reservation and cancellation policy varies by rate.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-lg font-medium">
+            Is there free breakfast at The King Hotel?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, The king hotel do offer complimentary breakfast.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-lg font-medium">
+           What are the check-in and check-out times at The King Hotel?
+          </div>
+          <div className="collapse-content">
+            <p>
+            Check-in at InterContinental Dhaka is from 3:00 PM, and check-out time is 12:00 PM. Contact the hotel directly for options available for early check-in or late check-out.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FQquestion;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import About from "../Components/About";
 import Bannar from "../Components/Bannar";
 import ContactUs from "../Components/ContactUs";
 import FeaturedRooms from "../Components/FeaturedRooms";
+import FQquestion from "../Components/FQquestion";
 import HappyCoustomer from "../Components/HappyCoustomer";
 import OurLocation from "../Components/OurLocation";
 import Reviews from "../Components/Reviews";
@@ -17,6 +18,7 @@ const Home = () => {
       <Reviews></Reviews>
       <OurLocation></OurLocation>
       <ContactUs></ContactUs>
+      <FQquestion></FQquestion>
       
     </div>
   );
